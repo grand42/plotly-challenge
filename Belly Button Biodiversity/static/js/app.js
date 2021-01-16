d8c8275c3ab0cb40398a0ccf4c2524310c47f2d8
Gretchen A <and01487@umn.edu>
2021-01-16 16:08:03 -0600

    Take top 10 values

--- Belly Button Biodiversity/static/js/app.js
+++ Belly Button Biodiversity/static/js/app.js
@@ -5,10 +5,17 @@ function buildPlot() {}
   d3.json("samples.json").then(function(importedData) {
       console.log(importedData);
       var samples = importedData.samples;
-      console.log(samples);
+      //console.log(samples);
       // filter by id
       var filtered_samples = importedData.samples.filter(sample => sample.id === "946")[0];
-      console.log(filtered_samples);
-      sample_id = filtered_samples.otu_ids;
-      console.log(sample_id);
+      //console.log(filtered_samples);
+      var sample_id = filtered_samples.otu_ids;
+      //console.log(sample_id);
+      var sample_value = filtered_samples.sample_values;
+      //console.log(sample_value);
+      //get top t0 values
+      var Values = sample_value.slice(0,10);
+      var IDs = sample_id.slice(0,10);
+      console.log(Values);
+      console.log(IDs);
     });
