b9f96f6c3db930a784a8b626cf6e797545c18d96
Gretchen A <and01487@umn.edu>
2021-01-16 16:00:09 -0600

    filter by id

--- Belly Button Biodiversity/static/js/app.js
+++ Belly Button Biodiversity/static/js/app.js
@@ -1,8 +1,14 @@
-
-// Fetch json data and console log
+// Create a plot for each Individual
 function buildPlot() {}
+// Fetch json data and console log
+
   d3.json("samples.json").then(function(importedData) {
       console.log(importedData);
       var samples = importedData.samples;
       console.log(samples);
+      // filter by id
+      var filtered_samples = importedData.samples.filter(sample => sample.id === "946")[0];
+      console.log(filtered_samples);
+      sample_id = filtered_samples.otu_ids;
+      console.log(sample_id);
     });
